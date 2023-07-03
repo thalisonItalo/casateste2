9d9467098d22fd27ba4fc2f1cb5e80c3028f8534
THALISON ITALO SANTANA DA COSTA <thalison.iscosta@senacsp.edu.br>
2023-07-03 09:56:13 -0300

    first commit

---
+++ tech.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 03/07/2023 às 05:58
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `tech`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `nome`, `email`, `senha`) VALUES
+(1, 'Thalison', 'teste@teste.com', 'teste');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `colaboradores`
+--
+
+CREATE TABLE `colaboradores` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `data_de_nascimento` date DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `sexo` varchar(10) DEFAULT NULL,
+  `cpf` varchar(11) DEFAULT NULL,
+  `endereco` varchar(100) DEFAULT NULL,
+  `n_endereco` varchar(10) DEFAULT NULL,
+  `cep` varchar(8) DEFAULT NULL,
+  `bairro` varchar(50) DEFAULT NULL,
+  `cidade` varchar(50) DEFAULT NULL,
+  `estado` varchar(50) DEFAULT NULL,
+  `senha` varchar(100) DEFAULT NULL,
+  `c_senha` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `colaboradores`
+--
+
+INSERT INTO `colaboradores` (`id`, `nome`, `data_de_nascimento`, `email`, `telefone`, `sexo`, `cpf`, `endereco`, `n_endereco`, `cep`, `bairro`, `cidade`, `estado`, `senha`, `c_senha`) VALUES
+(1, 'Thalison Italo santana da costa', '2023-07-25', 'thalisonitalo1@gmail.com', '11958883869', 'Feminino', '477.590.518', 'Avenida Santo Amaro', '0099', '04505-00', 'Vila Nova Conceição', 'Sao Paulo', 'São Paulo', '12345', '12345'),
+(2, 'Thalison Italo santana da costa', '2023-07-25', 'thalisonitalo1@gmail.com', '11958883869', 'Feminino', '477.590.518', 'Avenida Santo Amaro', '0099', '04505-00', 'Vila Nova Conceição', 'Sao Paulo', 'São Paulo', '12345', '12345');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `colaboradores`
+--
+ALTER TABLE `colaboradores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `colaboradores`
+--
+ALTER TABLE `colaboradores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
